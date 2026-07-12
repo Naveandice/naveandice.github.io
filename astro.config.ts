18b883e34561b9d2ac9475dcf8cfadde9bd34802
Naveandice <84022504+Naveandice@users.noreply.github.com>
2026-07-12 03:20:05 +0000

    feat: add redirect to fishstrap

--- astro.config.ts
+++ astro.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://naveandice.github.io",
+  redirects: {
+    "/fishstrap": "https://fishstrap.app",
+  },
 });
